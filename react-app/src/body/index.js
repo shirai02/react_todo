dfe058760d70c6a76ebf4633eafacea016b2cae8
macbookAir <kazuki@MacBook-Air.local>
2020-08-12 20:12:50 +0900

    ListItemとList用の関数作成

--- react-app/src/body/index.js
+++ react-app/src/body/index.js
@@ -1,14 +1,7 @@
 import React, { useState } from 'react';
-import Typography from '@material-ui/core/Typography';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
-import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
+import { Typography, List, ListItem, ListItemIcon, ListItemText, Divider, Checkbox } from '@material-ui/core';
+import { InboxIcon, DraftsIcon } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/core/styles';
-import Checkbox from '@material-ui/core/Checkbox';
 
 //ToDo Grid Layout使う
 //! classを使用しない
@@ -21,6 +14,10 @@ const useStyles = makeStyles((theme) => ({
         width: '100%',
         maxWidth: 900,
         backgroundColor: theme.palette.background.paper,
+        textAlign: 'center',
+    },
+    title: {
+        margin: theme.spacing(4, 0, 2),
     },
 }));
 
@@ -28,65 +25,49 @@ function ListItemLink(props) {
     return <ListItem button component="a" {...props} />;
 }
 
-function SimpleList() {
-    const [state, setState] = useState({
-        checkedA: false,
-        checkedB: false,
-        checkedF: false,
-    });
+function TodoListItem(props) {
 
-    const handleChange = name => event => {
-        setState({ ...state, [name]: event.target.checked });
-    };
+    const [checked, setChecked] = useState(false);
 
+    return (
+        <React.Fragment>
+            <ListItem>
+                <Checkbox
+                    checked={checked}
+                    onChange={() => setChecked(!checked)}
+                    value="checkedA"
+                    color="primary"
+                    // indeterminate
+                    inputProps={{
+                        'aria-label': 'primary checkbox',
+                    }}
+                />
+                <ListItemText primary={props.text} />
+            </ListItem>
+            <Divider />
+        </React.Fragment>
+    )
+}
+
+function TodoList() {
     const classes = useStyles();
+
     return (
         <div className={classes.root}>
-            <List component="nav">
-                <ListItem>
-                    <Checkbox
-                        checked={state.checkedA}
-                        onChange={handleChange('checkedA')}
-                        value="checkedA"
-                        // color="primary"
-                        // indeterminate
-                        inputProps={{
-                            'aria-label': 'primary checkbox',
-                        }}
-                    />
-                    <ListItemText primary="Inbox" />
-                </ListItem>
-                <Divider />
-                <ListItem>
-                    <ListItemText primary="Drafts" />
-                </ListItem>
-                <Divider />
-                <ListItem>
-                    <ListItemText primary="Trash" />
-                </ListItem>
-                <Divider />
-                <ListItem>
-                    <ListItemText primary="Spam" />
-                </ListItem>
+            <Typography variant='h6' className={classes.title}>
+                ToDo
+            </Typography>
+            <List
+                component="nav"
+            >
+                <TodoListItem text='test' />
+                <TodoListItem text='Drafts' />
+                <TodoListItem text='Trash' />
+                <TodoListItem text='Spam' />
+                {/* <TodoListItem /> */}
             </List>
         </div>
     );
 }
 
-class ToDo extends React.Component {
-    render() {
-        return (
-            <div className={useStyles.root}>
-                <Typography variant="body1">
-                    body1
-                </Typography>
-                <Typography variant="body2">
-                    body2
-                </Typography>
-            </div>
-        );
-    }
-}
-
-// export default ToDo;
-export default SimpleList;
+export default TodoList;
